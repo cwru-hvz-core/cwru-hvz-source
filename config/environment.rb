7b7da3bc1daaf1a4afac5a95d48858103f895b95
Tom Dooner <tomdooner@gmail.com>
2011-02-28 15:19:29 -0500

    Allegedly fixes the CSS compiling issue.

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,11 @@ require File.expand_path('../application', __FILE__)
 require 'casclient'
 require 'casclient/frameworks/rails/filter'
 require 'googlevoiceapi'
+require 'hassle'
 require './lib/update_game_state.rb'
 require 'google_chart'
+
+Sass::Plugin.options[:template_location] = { "/app/stylesheets" => "public/stylesheets" }
 
 CASClient::Frameworks::Rails::Filter.configure(
       :cas_base_url => "https://login.case.edu/cas/"
