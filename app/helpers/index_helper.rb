8050e7a917138a81a70f78f6dac3b09ccbb28f46
Tom Dooner <tomdooner@gmail.com>
2011-03-23 03:05:18 -0400

    Adds a kind-of-boring yet kind-of-interesting feed of kills to the right on the homepage.

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -1,2 +1,15 @@
 module IndexHelper
+	def tag_text(tag)
+		tagger = tag.tagger.person
+		tagger_str = "An unknown zombie"
+		unless tagger.nil?
+			tagger_str = tagger.name
+		end
+		if tag.tagger.is_oz and not @current_game.ozs_revealed?
+			tagger_str = "An original zombie"
+		end
+		tagee = tag.tagee.person
+
+		tagger_str + " tagged " + tagee.name + " " + distance_of_time_in_words(tag.datetime - @current_game.utc_offset, Time.now) + " ago."
+	end
 end
