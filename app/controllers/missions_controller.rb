d5b070458c66273dbd297db96f7f7926cdc290fb
Tom Dooner <tomdooner@gmail.com>
2011-03-24 01:51:33 -0400

    Halfway through mission implementation, but fixes a small oopsie.

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,20 +1,25 @@
 class MissionsController < ApplicationController
 	before_filter :check_admin, :only => [:new, :create, :list, :edit, :update]
+#	autocomplete :registration, :name
 
   def new
 	  @mission = Mission.new
+  end
+
+  def attendance
+  	  @mission = Mission.find(params[:id], :include=>:attendances)
+	  @attendance = @mission.attendances.new
   end
 
   def create
 	  @mission = Mission.new(params[:mission])
 	  @mission.game = @current_game
 	  if @mission.save()
-		  redirect_to mission_list_url()
+		  redirect_to list_mission_url()
 	  else 
 		  flash[:error] = @mission.errors.full_messages.first
 		  redirect_to new_mission_url()
 	  end
-
   end
 
   def index	
