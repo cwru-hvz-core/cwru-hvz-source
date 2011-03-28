5b7642ecf61e2b92d27400665383cbd56f64acf1
Tom Dooner <tomdooner@gmail.com>
2011-03-27 23:00:55 -0400

    Fixes text on homepage for administrative tags.

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -1,15 +1,15 @@
 module IndexHelper
 	def tag_text(tag)
-		tagger = tag.tagger.person
-		tagger_str = "An unknown zombie"
+		tagger = tag.tagger
 		unless tagger.nil?
-			tagger_str = tagger.name
+			tagger_str = tagger.person.name + " tagged "
+			if tag.tagger.is_oz and not  @current_game.ozs_revealed?
+				tagger_str = "An original zombie tagged "
+			end
 		end
-		if tag.tagger.is_oz and not @current_game.ozs_revealed?
-			tagger_str = "An original zombie"
-		end
+		tagger_str ||= "An administrator converted "
 		tagee = tag.tagee.person
 
-		tagger_str + " tagged " + tagee.name + " " + distance_of_time_in_words(tag.datetime - @current_game.utc_offset, Time.now) + " ago."
+		tagger_str + tagee.name + " " + distance_of_time_in_words(tag.datetime - @current_game.utc_offset, Time.now) + " ago."
 	end
 end
