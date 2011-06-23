6ad409b94b1a38bef68d3303d7e83b77fefdbb52
Tom Dooner <tomdooner@gmail.com>
2011-06-22 23:35:49 -0400

    Adds the list of Recently Tagged players to the homepage.

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   thisgame.append_on_load_info("players", function() {
+    
+    // Update the "Game Statistics" box on the top right of the page
     thisgame.load_players();
     counts = {};
     for (var i in thisgame.players) {
@@ -12,5 +14,22 @@ $(document).ready(function() {
     $("a#count_zombies").html("Zombies: " + counts["1"]);
     $("a#count_deceased").html("Deceased: " + counts["2"]);
     $("a#count_total").html("Total: " + thisgame.players.length);
+
+    // Update the "Recently Tagged" box on the bottom right
+    thisgame.append_on_load_info("tags", function() {
+      var t = thisgame.retrieved_data["tags"].sort( function(a,b) { return new Date(b.datetime) - new Date(a.datetime) } );
+      $("ul#recently_tagged").html("");
+      var i = 0;
+      for (var tag in t) {
+        console.log(t[tag]);
+        $("ul#recently_tagged").append("<li><a href='/players/" + t[tag].tagged_id + "'>" + thisgame.players_by_id[t[tag].tagged_id].name + "</a></li>");
+        i += 1;
+        if (i >= 10 && (new Date(thisgame.now) - new Date(t[tag].datetime))/1000 > 3600) {
+          break;
+        }
+      }
+    });
+
+
   });
 });
