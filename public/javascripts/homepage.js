400333a93b56c19bedf95b7a04f5f4b071fce5d1
Tom Dooner <tomdooner@gmail.com>
2011-06-24 00:17:40 -0400

    A few tweaks for Safari.

--- public/javascripts/homepage.js
+++ public/javascripts/homepage.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
   thisgame.append_on_load_info("players", function() {
-    
-    // Update the "Game Statistics" box on the top right of the page
-    thisgame.load_players();
-    counts = {};
-    for (var i in thisgame.players) {
-      if (!(thisgame.players[i].faction.key in counts)) {
-        counts[thisgame.players[i].faction.key] = 0;
+   
+    thisgame.append_on_load_info("info", function() {
+      // Update the "Game Statistics" box on the top right of the page
+      thisgame.load_players();
+      counts = {};
+      for (var i in thisgame.players) {
+        if (!(thisgame.players[i].faction.key in counts)) {
+          counts[thisgame.players[i].faction.key] = 0;
+        }
+        counts[thisgame.players[i].faction.key] += 1;
       }
-      counts[thisgame.players[i].faction.key] += 1;
-    }
-    $("a#count_humans").html("Humans: " + counts["0"]);
-    $("a#count_zombies").html("Zombies: " + counts["1"]);
-    $("a#count_deceased").html("Deceased: " + counts["2"]);
-    $("a#count_total").html("Total: " + thisgame.players.length);
+      $("a#count_humans").html("Humans: " + counts["0"]);
+      $("a#count_zombies").html("Zombies: " + counts["1"]);
+      $("a#count_deceased").html("Deceased: " + counts["2"]);
+      $("a#count_total").html("Total: " + thisgame.players.length);
+    });
 
     // Update the "Recently Tagged" box on the bottom right
     thisgame.append_on_load_info("tags", function() {
