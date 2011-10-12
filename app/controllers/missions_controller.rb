c51b34ab9bebabec18a3540c55534c6ed682407e
Tom Dooner <tomdooner@gmail.com>
2011-10-12 16:14:41 -0400

    Make the feed input screen a bit more useful, hopefully.

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -17,9 +17,10 @@ class MissionsController < ApplicationController
   def feeds
   	@mission = Mission.find(params[:id])
 	  @feeds = @mission.feeds.sort{|x,y| y.created_at <=> x.created_at}
-    @all_zombies = @mission.attendances.map{|x| x.registration_id if x.registration.is_zombie?}.compact
-    @fed_players = @feeds.map{|x| x.registration_id}
-    @need_feeding = Registration.find(@all_zombies - @fed_players).sort{|x,y| x.score <=> y.score}
+    @all_zombies = Registration.find_all_by_game_id(@current_game).keep_if{|x| x.is_zombie? or x.is_deceased?}
+    @present_zombies = @mission.attendances.map{|x| x.registration if @all_zombies.include?(x.registration)}.compact
+    @fed_players = @feeds.map{|x| x.registration}
+    @need_feeding = (@all_zombies - @fed_players).sort_by{|x| [ x.state_history[:deceased], x.score]}.keep_if{|x| Game.now(@current_game) + @current_game.utc_offset - x.state_history[:deceased] < -2.hours}
 	  @feed = Feed.new({:mission => @mission})
   end
 
