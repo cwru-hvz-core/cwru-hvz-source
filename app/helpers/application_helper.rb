7b1b1b33f20f79d6a53392bbe335b60c6747c6f4
Tom Dooner <tom@causes.com>
2012-09-28 17:58:26 -0400

    Add Gravatar for users at their default abc123@case.edu address.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
 	def date_humanize_dayofweek_and_time(date)
 		return date.strftime("%A, %I:%M %p")
 	end
+
+  def profile_photo_url(person, size=32, default=image_path('gravatar_default.png'))
+    ["http://www.gravatar.com/avatar/",
+      Digest::MD5.hexdigest(person.caseid + '@case.edu'),
+      "?s=#{size}&d=#{u default}"
+    ].join
+  end
+
+  def gravatar_profile_url(person)
+    ["http://www.gravatar.com/",
+      Digest::MD5.hexdigest(person.caseid + '@case.edu'),
+    ].join
+  end
 end
