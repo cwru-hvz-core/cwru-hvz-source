f6abd5f6f44ed492cf2c67150280fa70c1966770
Tom Dooner <tom@causes.com>
2012-10-05 01:40:24 -0400

    Add mass feed functionality

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,6 +2,20 @@ class FeedsController < ApplicationController
 	before_filter :check_admin
 
   def create
+    if params[:mass_feed] == 'true' && params[:mission_id]
+      @mission = Mission.find(params[:mission_id])
+
+      @current_game.registrations.where(:faction_id => 1).each do |r| # All current zombies
+        @feed = Feed.new
+        @feed.mission = @mission
+        @feed.datetime = @mission.end
+        @feed.registration = r
+        @feed.save()
+      end
+
+      return redirect_to mission_feeds_url(@mission)
+    end
+
 	  @feed = Feed.new(params[:feed])
       if @feed.person_id.nil?
           @feed.person_id = Person.where("lower(name) LIKE ?", params[:feed][:person_name].downcase + "%")
