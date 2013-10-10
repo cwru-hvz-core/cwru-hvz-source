6584bedbdde1a60f9c8434b93d7f73644be91717
Tom Dooner <tom@causes.com>
2013-10-09 20:17:44 -0700

    Add some fragment caching back in to GameController#show

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,8 +7,11 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.where(id: params[:id]).first
+
     return if !stale?(@game)
 
+    # Save the costly DB lookups if the stuff is cached already:
+    return render if Rails.cache.exist? ['views', @game.cache_key, 'show'].join('/')
     @game = Game.where(id: params[:id]).
       includes(squads: :registrations, registrations: [
         :taggedby, :tagged, :feeds, :game, :person, :infractions,
