be7f829c76b3285ee4d29bf414ee6a0325b91277
Tom Dooner <tom.dooner@causes.com>
2013-10-20 23:08:09 -0700

    Sort the autocomplete list better
    It now will assume you meant the person's first name.

--- app/assets/javascripts/backbone/collections/playerList.js
+++ app/assets/javascripts/backbone/collections/playerList.js
@@ -1,13 +1,16 @@
 HvZ.PlayerList = Backbone.Collection.extend({
   model: HvZ.Player,
   search : function(q) {
-    if (q === '' || q === undefined || q.length < 2) {
+    if (q === '' || q === undefined || q.length <= 1) {
       return [];    // prevent visual clutter
     }
 
-    return _.filter(this.models, function(player) {
-      // todo: sort by edit distance?
-      return player.get('name').toLowerCase().indexOf(this.q.toLowerCase()) >= 0;
+    return _.sortBy(_.filter(this.models, function(player) {
+      return _.some(player.get('name').toLowerCase().split(' '), function(chunk) {
+        return chunk.indexOf(this.q.toLowerCase()) == 0;
+      }, { q : q });
+    }, { q : q }), function(p) {
+      return p.get('name').toLowerCase().indexOf(this.q.toLowerCase());
     }, { q : q });
   },
 });
