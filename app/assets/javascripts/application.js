4a33e3bcdf1cc327ba7d37e4cfeb3f587a46bf7d
Aaron Neyer <aaronneyer@gmail.com>
2014-03-25 17:22:56 -0400

    move backbone for player list out of application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,3 @@
 //= require jquery_ujs
 //= require jquery.countdown.min
 //= require jit
-//= require backbone/models/player
-//= require backbone/collections/playerList
-//= require backbone/views/attendingPlayers
-//= require backbone/views/playerAutocomplete
