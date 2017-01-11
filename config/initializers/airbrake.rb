4a20229727cc80f025f62b950a19b20c0ec092e2
James Hochadel <james.hochadel@gmail.com>
2017-01-11 16:29:45 -0500

    Airbrake now sets on only in prod environment

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,7 @@
-Airbrake.configure do |config|
-  config.project_id = '87317'
-  config.project_key = '45eb1d65ac2a60c0bab6f70d216d1dee'
+# We don't need airbrake emails in dev and test
+if Rails.env.production?
+  Airbrake.configure do |config|
+    config.project_id = '87317'
+    config.project_key = '45eb1d65ac2a60c0bab6f70d216d1dee'
+  end
 end
