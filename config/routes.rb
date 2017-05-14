9d133fe048695ca6f08984ea2ba3a703f51027ba
James Hochadel <james.hochadel@gmail.com>
2017-05-14 14:57:37 -0400

    Update to Rails 4 and get server running

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Hvz::Application.routes.draw do
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
-  	match "/people/login/" => "people#login"
-  	match "/people/logout/" => "people#logout"
-	resources :people, :tags, :feeds
-	resources :attendances
+  match "/people/login/", to: "people#login", via: :all
+  match "/people/logout/", to: "people#logout", via: :all
+  resources :people, :tags, :feeds
+  resources :attendances
   get "bonus_codes/claim" => "bonus_codes#claim", :as => "claim_bonus_code"
   put "bonus_codes/claim" => "bonus_codes#claim_submit", :as => "submit_bonus_code"
   resources :bonus_codes
@@ -26,10 +26,10 @@ Hvz::Application.routes.draw do
       post 'points' => 'missions#save_points'
     end
   end
-	resources :players, :as => :registrations, :controller => :registrations, :id=>/[0-9]*/ do
+  resources :players, :as => :registrations, :controller => :registrations, :id=>/[0-9]*/ do
       get 'showwaiver'
-		resources :infractions
-	end
+    resources :infractions
+  end
   match '/players/find_by_code/' => 'registrations#find_by_code', :as => 'find_registration_by_code', :via => 'get'
   get 'players/:id/waiver' => "waiver#new", :as => "sign_waiver"
   post 'players/:id/waiver' => "waiver#create", :as => "waivers"
@@ -53,67 +53,19 @@ Hvz::Application.routes.draw do
   end
 
   get '/contact' => 'contact_messages#new'
-	resources :contact, :as => :contact_messages, :controller => :contact_messages do
-		collection do
-			get :list
-		end
-	end
+  resources :contact, :as => :contact_messages, :controller => :contact_messages do
+    collection do
+      get :list
+    end
+  end
 
-  match "squads/:id" => "squad#show", :as=> "squad"
-  match "squads" => "squad#index", :as=> "squads"
-  match "players/:id/joinsquad/:squadid" => "registrations#joinsquad", :as => "join_squad"
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  match "squads/:id", to: "squad#show", as: "squad", via: :all
+  match "squads", to: "squad#index", as: "squads", via: :all
+  match "players/:id/joinsquad/:squadid", to: "registrations#joinsquad", as: "join_squad", via: :all
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => "index#root"
 
   # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
